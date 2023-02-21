ae1d6ceeac4c1f6edeb50f784d2d62e40a5b0446
mijinkoo <starmj09@naver.com>
2023-02-21 10:11:42 +0900

    [FTTS-54] weekly 그래프 위젯 추가

--- ftts_flutter/lib/widget/WeeklyGraph.dart
+++ ftts_flutter/lib/widget/WeeklyGraph.dart
@@ -35,11 +35,6 @@ class _WeeklyGraphState extends State<WeeklyGraph> {
   Widget build(BuildContext context) {
     double screenWidth = MediaQuery.of(context).size.width;
 
-    final List<DoughnutChartData> doughnutChartData = [
-      DoughnutChartData('섭취한 칼로리', 1700, Color(0xFF3617CE)),
-      DoughnutChartData('남은 칼로리', 400, Color(0xFFe8e8e8)),
-    ];
-
     List<VBarChartModel> barChartData = [
       const VBarChartModel(
           index: 0,
@@ -47,6 +42,21 @@ class _WeeklyGraphState extends State<WeeklyGraph> {
           jumlah: 83,
           tooltip: "83%",
           label: '칼로리'),
+      const VBarChartModel(
+          index: 1,
+          colors: [Colors.deepOrange, Colors.red],
+          jumlah: 100,
+          tooltip: "152%",
+          label: '탄수화물'),
+      const VBarChartModel(
+          index: 3,
+          colors: [Colors.limeAccent, Colors.yellow],
+          jumlah: 89,
+          tooltip: "89%",
+          label: '지방'),
+    ];
+
+    List<VBarChartModel> barChartData2 = [
       const VBarChartModel(
           index: 1,
           colors: [Colors.deepOrange, Colors.red],
@@ -65,6 +75,18 @@ class _WeeklyGraphState extends State<WeeklyGraph> {
           jumlah: 89,
           tooltip: "89%",
           label: '지방'),
+      const VBarChartModel(
+          index: 3,
+          colors: [Colors.limeAccent, Colors.yellow],
+          jumlah: 89,
+          tooltip: "89%",
+          label: '비타민'),
+      const VBarChartModel(
+          index: 3,
+          colors: [Colors.limeAccent, Colors.yellow],
+          jumlah: 89,
+          tooltip: "89%",
+          label: '무기질'),
     ];
 
     List<StackedBarChartData> stackedBarChartData = [
@@ -77,73 +99,44 @@ class _WeeklyGraphState extends State<WeeklyGraph> {
       width: screenWidth * 0.9,
       margin: EdgeInsets.all(0),
       color: Colors.white,
-      // color: Color(0xFFF4F6F9),
-      child: Row(
-        children: [
-          Container(
-              child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Container(
-                margin: EdgeInsets.only(top: 20.0),
-                child: Text(
-                  "남은 칼로리",
-                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
-                ),
-              ),
-              Container(
-                height: 150,
-                width: 150,
-                child: SfCircularChart(
-                    centerY: '80',
-                    margin: EdgeInsets.zero,
-                    annotations: <CircularChartAnnotation>[
-                      CircularChartAnnotation(
-                          widget: Container(
-                        child: const Text(
-                          "1700kcal",
-                          style: TextStyle(
-                              // fontWeight: FontWeight.bold,
-                              fontSize: 18),
-                        ),
-                      ))
-                    ],
-                    series: <CircularSeries>[
-                      DoughnutSeries<DoughnutChartData, String>(
-                          dataSource: doughnutChartData,
-                          pointColorMapper: (DoughnutChartData data, _) =>
-                              data.color,
-                          xValueMapper: (DoughnutChartData data, _) => data.x,
-                          yValueMapper: (DoughnutChartData data, _) => data.y,
-                          innerRadius: '70%')
-                    ]),
-              ),
-            ],
-          )),
-          SingleChildScrollView(
-              scrollDirection: Axis.horizontal,
-              child: Container(
-                  width: screenWidth * 0.50,
-                  child: VerticalBarchart(
+      child: SingleChildScrollView(
+        scrollDirection: Axis.horizontal,
+        child: Row(
+          children: [
+            Container(
+                width: screenWidth * 0.45,
+                child: VerticalBarchart(
                     background: Colors.transparent,
                     labelColor: Colors.black,
-                    labelSizeFactor: 0.45,
+                    labelSizeFactor: 0.48,
                     tooltipColor: Colors.black,
-                    data: barChartData,
+                    data: barChartData2,
                     showBackdrop: true,
                     barStyle: BarStyle.DEFAULT,
                     barSize: 12,
                     maxX: 100,
-                    showLegend: true,
-                    legend: [
-                      Vlegend(isSquare: false, color: Colors.green, text: "적정"),
-                      Vlegend(isSquare: false, color: Colors.red, text: "과잉"),
-                      Vlegend(
-                          isSquare: false, color: Colors.yellow, text: "부족"),
-                    ],
-                  ))),
-        ],
+                    showLegend: false)),
+            Container(
+                width: screenWidth * 0.45,
+                child: VerticalBarchart(
+                  background: Colors.transparent,
+                  labelColor: Colors.black,
+                  labelSizeFactor: 0.48,
+                  tooltipColor: Colors.black,
+                  data: barChartData,
+                  showBackdrop: true,
+                  barStyle: BarStyle.DEFAULT,
+                  barSize: 12,
+                  maxX: 100,
+                  showLegend: true,
+                  legend: [
+                    Vlegend(isSquare: false, color: Colors.green, text: "적정"),
+                    Vlegend(isSquare: false, color: Colors.red, text: "과잉"),
+                    Vlegend(isSquare: false, color: Colors.yellow, text: "부족"),
+                  ],
+                )),
+          ],
+        ),
       ),
     );
   }
